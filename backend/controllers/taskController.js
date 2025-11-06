0e1315459bf96a5946647bdf9fddeb7157ec41e1
SouzaHyago <hyago.souza0103@gmail.com>
2025-11-06 17:31:21 -0300

    adicao deletar tarefas e listar tarefas

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -9,3 +9,31 @@ export async function createTask(req, res) {
     res.status(400).json({ error: err.message });
   }
 }
+
+export async function listTasks(req, res) {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Erro ao listar tarefas", details: err.message });
+  }
+}
+
+export async function deleteTask(req, res) {
+  try {
+    const { id } = req.params;
+    const deletedTask = await Task.findByIdAndDelete(id);
+
+    if (!deletedTask) {
+      return res.status(404).json({ error: "Tarefa não encontrada" });
+    }
+
+    res.status(200).json({ message: "Tarefa excluída com sucesso" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Erro ao excluir tarefa", details: err.message });
+  }
+}
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import {
-  createTask
+  createTask,
   listTasks,
   updateTask,
   deleteTask,
